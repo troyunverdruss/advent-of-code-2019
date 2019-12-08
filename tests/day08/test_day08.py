011328b2f913af4feed5a0c1d51f56281058a01f
Troy Unverdruss <troy@scroll.com>
2019-12-07 21:55:26 -0800

    Day 8 in the bag, woo

---
+++ tests/day08/test_day08.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from ddt import data, ddt, unpack
+
+from days.day08.day08 import part1, part2_read_into_buffer
+
+
+class TestDay08(TestCase):
+    def test_part_1(self):
+        _input = list(map(int, "121212789012"))
+        self.assertEqual(9, part1(3, 2, _input))
+
+    def test_part_2(self):
+        _input = list(map(int, "0222112222120000"))
+        buffer = part2_read_into_buffer(2, 2, _input)
+        self.assertEqual(0, buffer[(0, 0)])
+        self.assertEqual(1, buffer[(1, 0)])
+        self.assertEqual(1, buffer[(0, 1)])
+        self.assertEqual(0, buffer[(1, 1)])
