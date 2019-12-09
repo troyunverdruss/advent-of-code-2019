cb39d317a53bfdfb585bef13b728b238f7b44d93
Troy Unverdruss <troy@scroll.com>
2019-12-09 08:38:25 -0800

    Finishing up a little cleanup/commenting

--- days/day06/day06.py
+++ days/day06/day06.py
@@ -1,8 +1,10 @@
 from collections import deque
+from typing import List, Dict
 
 from helpers import read_raw_entries
 
 
+# For use with the search-based approach
 class Node:
     def __init__(self, id):
         self.id = id
@@ -10,6 +12,8 @@ class Node:
         self.children = []
 
 
+# Simple recursive function to count links from
+# any node to "COM"
 def count_links_to_com(nodes, starting_id):
     if nodes[starting_id] == "COM":
         return 1
@@ -17,6 +21,8 @@ def count_links_to_com(nodes, starting_id):
     return 1 + count_links_to_com(nodes, nodes[starting_id])
 
 
+# Simple recursive function to create a full list
+# of all the links from start to "COM"
 def find_path_to_com(nodes, starting_id):
     if nodes[starting_id] == "COM":
         return [starting_id, "COM"]
@@ -24,6 +30,7 @@ def find_path_to_com(nodes, starting_id):
     return [starting_id] + find_path_to_com(nodes, nodes[starting_id])
 
 
+# Compute the total of all the links
 def part1(lines):
     nodes = parse_nodes(lines)
 
@@ -33,31 +40,50 @@ def part1(lines):
     return total
 
 
-def parse_nodes(lines):
-    nodes = {}
+# Parse the input data into a relationship map
+def parse_nodes(lines: List[str]) -> Dict[str, str]:
+    # Relationship map. Parent => Child that orbits parent
+    relationship_map = {}
     for line in lines:
         parent, orbiter = line.split(")")
-        nodes[orbiter] = parent
-    return nodes
+        relationship_map[orbiter] = parent
+
+    return relationship_map
 
 
+# This turns out to be a terribly inefficient way of getting
+# this solution, but it *does* work. Basically do a BFS from
+# YOU to SAN, pruning away possibilities as we go.
+# Note: since this is a proper tree, with no weird or bad
+# directed links, using sets (as below) is much faster and simpler.
 def part2_with_search(lines):
-    relationships = parse_nodes(lines)
+    relationship_map = parse_nodes(lines)
     nodes = {"COM": Node("COM")}
 
-    for node in relationships.keys():
+    # Build up a list of all the Node objects
+    for node in relationship_map.keys():
         nodes[node] = Node(node)
-    for node in relationships.keys():
-        nodes[node].parent = nodes[relationships[node]]
+    # Then link them all up so they know their parents
+    # and children
+    for node in relationship_map.keys():
+        nodes[node].parent = nodes[relationship_map[node]]
         nodes[node].parent.children.append(nodes[node])
 
-    start = relationships["YOU"]
-    dest = relationships["SAN"]
+    # We're starting at the node that YOU is orbiting
+    start = relationship_map["YOU"]
+    # and ending at the node that SAN is orbiting
+    dest = relationship_map["SAN"]
 
-    open = deque()
-    open.append(start)
-    steps_needed = {start: 0}
+    # queue will track points we need to search
+    # and closed will track points we've visited
+    queue = deque()
     closed = set()
+
+    # Seed it with the start
+    queue.append(start)
+    # Use this map to track how many steps each place
+    # we've visited took to get there
+    steps_needed = {start: 0}
 
     iterations = 0
     while True:
@@ -65,30 +91,45 @@ def part2_with_search(lines):
         # print(f"Open: {len(open)}, Closed: {len(closed)}, Iterations: {iterations}")
         iterations += 1
 
-        current = open.popleft()
+        # Get the next place to search from
+        current = queue.popleft()
+        # Update the steps map for this node
         steps = steps_needed[current]
 
+        # Move it to closed, now that we're looking at it
         closed.add(current)
 
+        # Did we get there?
         if current == dest:
             return steps
 
+        # Get the actual node object
         current_node = nodes[current]
 
+        # If we have a parent and the parent hasn't been searched
+        # and isn't scheduled for search already, queue it up
         if (
-            current_node.parent
-            and current_node.parent.id not in closed
-            and current_node.parent.id not in open
+                current_node.parent
+                and current_node.parent.id not in closed
+                and current_node.parent.id not in queue
         ):
-            open.append(current_node.parent.id)
+            queue.append(current_node.parent.id)
             steps_needed[current_node.parent.id] = steps + 1
 
+        # For each child node, do the same check and queue it up
+        # if it hasn't been seen before
         for child in current_node.children:
-            if child.id not in closed and child.id not in open:
-                open.append(child.id)
+            if child.id not in closed and child.id not in queue:
+                queue.append(child.id)
                 steps_needed[child.id] = steps + 1
 
 
+# Here's the much simpler approach ... get the full list of
+# nodes between YOU and COM, then from SAN to COM, cast them
+# to sets, and do a symmetric difference to find all the nodes
+# that are not shared. This is the path from YOU to some shared
+# parent, and from SAN to the same shared parent. Nodes in this
+# set are the same number of steps needed. Tada!
 def part2_with_sets(lines):
     orbiter_to_parent_map = parse_nodes(lines)
     # We don't want to include YOU or SAN, so we're starting with each of their direct ancestors
--- days/day07/day07.py
+++ days/day07/day07.py
@@ -5,6 +5,7 @@ from days.day02.intcode_computer import IntcodeComputer
 from helpers import read_raw_entries
 
 
+# We're just gonna brute force try all the possible phases
 def part1(instructions):
     max_output = 0
     for permutation in permutations(range(5)):
@@ -16,6 +17,7 @@ def part1(instructions):
     return max_output
 
 
+# Try each permutation
 def run_part1_with_permutation(instructions, permutation):
     input_value = 0
     for phase_setting in permutation:
@@ -26,6 +28,7 @@ def run_part1_with_permutation(instructions, permutation):
     return input_value
 
 
+# Same deal, just brute forcing it
 def part2(instructions):
     max_output = 0
     for permutation in permutations(range(5, 10)):
@@ -39,20 +42,38 @@ def part2(instructions):
     return max_output
 
 
+# A little trickier on part 2, need to keep trying until they all halt
 def run_part2_with_permutation(instructions, permutation):
+    # Set up an IC for each letter in a list
     ics = []
     for index, _id in enumerate(list("ABCDE")):
         ic = IntcodeComputer(instructions, _id)
         ic.set_inputs([permutation[index]])
         ics.append(ic)
 
+    # Seed A with 0
     ics[0].inputs.append(0)
 
+    # We need to know what the final output is
     last_output = 0
+
+    # I don't want to over-complicate the logic to get *into* the loop the
+    # first time, or put all the checks at the end
     start = True
 
+    # Running has a specific meaning here, and that is that it hasn't *halted*. A
+    # computer can be paused waiting for input, but it since it has not halted, it is
+    # still "running". So as long as any of the ics are running, keep churning through.
+    # An ic in a "paused" state won't proceed to do anything until it has received input
+    # so you can call run() on it as many times as you want
     while start or any(map(lambda c: c.is_running(), ics)):
+        # You can only start once
         start = False
+
+        # Run every ic. Paused ics will just skip quickly,
+        # unpaused ones will run, produce output, then we'll
+        # stuff it into the next one's input for it to consume
+        # when it runs again
         for index, ic in enumerate(ics):
             ic.run()
             output = ic.get_outputs().pop()
