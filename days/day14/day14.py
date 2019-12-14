aa17c4605ea43c19d421e75e28b35cd8414526c3
Troy Unverdruss <troy@scroll.com>
2019-12-14 15:43:39 -0800

    everything but 1 test case passing on day 14. i think there's a subtle bug remaining

--- days/day14/day14.py
+++ days/day14/day14.py
@@ -28,7 +28,7 @@ def parse_lines(_lines):
     ore_producers = {}
 
     for l in _lines:
-        print(l)
+        # print(l)
         inputs, outputs = l.split(" => ")
 
         needed_in = []
@@ -52,26 +52,38 @@ def parse_lines(_lines):
         if len(needed_in) == 1 and "ORE" == needed_in[0].element:
             ore_producers[out_el.element] = reaction_step
 
-    for e in reaction_steps.keys():
-        print(f"{reaction_steps[e].reaction_in} => {reaction_steps[e].reaction_out}")
-    i = 0
+    # for e in reaction_steps.keys():
+    #     print(f"{reaction_steps[e].reaction_in} => {reaction_steps[e].reaction_out}")
+    # i = 0
 
     return reaction_steps, ore_producers
 
 
-def part1(lines):
+def part1(lines, fuel_count=None, leftover_pool=None):
     reaction_steps, ore_producers = parse_lines(lines)
+
+    if leftover_pool is None and fuel_count is not None:
+        raise Exception("Shouldn't have specified fuel count without providing reusable pool")
+
+    # If we weren't passed one in, init it now
+    if leftover_pool is None:
+        leftover_pool = defaultdict(lambda: 0)
+
+    if fuel_count is None:
+        fuel_count = 1
+
     needed_inputs = find_inputs(
-        reaction_steps, ore_producers, "FUEL", 1, defaultdict(lambda: 0)
+        reaction_steps, ore_producers, "FUEL", fuel_count, leftover_pool
     )
 
-    print(needed_inputs)
+    print("leftover pool", leftover_pool)
 
     ore = 0
     for k in needed_inputs.keys():
-        multiplier = 1
-        while ore_producers[k].reaction_out.quantity * multiplier < needed_inputs[k]:
-            multiplier += 1
+        multiplier = int(math.ceil(float(needed_inputs[k] / ore_producers[k].reaction_out.quantity)))
+        # multiplier = 1
+        # while ore_producers[k].reaction_out.quantity * multiplier < needed_inputs[k]:
+        #     multiplier += 1
         # multiplier = int(math.ceil(float(needed_inputs[k])/float(ore_producers[k].reaction_in[0].quantity)))
 
         ore += multiplier * ore_producers[k].reaction_in[0].quantity
@@ -84,13 +96,41 @@ def part1(lines):
     # 2486514
 
 
+def part2(lines):
+    trillion = 1_000_000_000_000
+    approx_ore_per_one_fuel = part1(lines)
+
+    ore_remaining = trillion
+    count = 0
+    leftover_pool = defaultdict(lambda: 0)
+
+    while ore_remaining // approx_ore_per_one_fuel > 0:
+        fuel_to_create = ore_remaining // approx_ore_per_one_fuel
+        ore_used = part1(lines, fuel_to_create, leftover_pool)
+        if ore_used > ore_remaining:
+            raise Exception()
+        ore_remaining -= ore_used
+        count += fuel_to_create
+
+    while ore_remaining > 0:
+        _used = part1(lines, 1, leftover_pool)
+        if _used > ore_remaining:
+            break
+        else:
+            ore_remaining -= _used
+            count += 1
+
+    print(count)
+    return count
+
+
 def find_inputs_2(
-    reaction_steps: Dict[str, ReactionStep],
-    ore_producers: Dict[str, ReactionStep],
-    required_element: str,
-    quantity: int,
-    pool: Dict[str, int],
-    indent=0,
+        reaction_steps: Dict[str, ReactionStep],
+        ore_producers: Dict[str, ReactionStep],
+        required_element: str,
+        quantity: int,
+        pool: Dict[str, int],
+        indent=0,
 ):
     if required_element in ore_producers:
         total_produced = math.ceil(float(quantity / ore_producers[required_element].reaction_out.quantity))
@@ -106,16 +146,13 @@ def find_inputs_2(
         return {}
 
 
-
-
-
 def find_inputs(
-    reaction_steps: Dict[str, ReactionStep],
-    ore_producers: Dict[str, ReactionStep],
-    required_element: str,
-    quantity: int,
-    pool: Dict[str, int],
-    indent=0,
+        reaction_steps: Dict[str, ReactionStep],
+        ore_producers: Dict[str, ReactionStep],
+        required_element: str,
+        quantity: int,
+        pool: Dict[str, int],
+        indent=0,
 ):
     # print(f"{'  ' * indent}Seeking {quantity} of element: {required_element}")
     # base case is ORE?
@@ -141,16 +178,27 @@ def find_inputs(
     reaction = reaction_steps[required_element]
 
     # multiplier = int(math.ceil(float(reaction.reaction_out.quantity)/float(quantity)))
+    min_multiplier = quantity // reaction.reaction_out.quantity
+    if min_multiplier * reaction.reaction_out.quantity < quantity:
+        if min_multiplier * reaction.reaction_out.quantity + pool[reaction.reaction_out.element] >= quantity:
+            # to_sub = quantity - (min_multiplier * reaction.reaction_out.quantity)
+            # pool[reaction.reaction_out.element] -= to_sub
+            pass
+        else:
+            min_multiplier += 1
 
-    multiplier = 1
-    while (
-        reaction.reaction_out.quantity * multiplier
-        + pool[reaction.reaction_out.element]
-        < quantity
-    ):
-        multiplier += 1
-
-    # take out of the pool as much as necessary
+    multiplier = min_multiplier
+    # multiplier = 1
+    # while (
+    #         reaction.reaction_out.quantity * multiplier
+    #         + pool[reaction.reaction_out.element]
+    #         < quantity
+    # ):
+    #     multiplier += 1
+    #
+    # print(f"min_multiplier: {min_multiplier}, expected: {multiplier}")
+    #
+    # # take out of the pool as much as necessary
     to_create = reaction.reaction_out.quantity * multiplier
     if to_create > quantity:
         pool[reaction.reaction_out.element] += to_create - quantity
@@ -163,9 +211,9 @@ def find_inputs(
     # pool[reaction.reaction_out.element] = new_val
 
     # deduct from the pool
-    print("pool:")
-    for e in filter(lambda e: e[1] != 0, pool.items()):
-        print(e)
+    # print("pool:")
+    # for e in filter(lambda e: e[1] != 0, pool.items()):
+    #     print(e)
 
     for el_in in reaction.reaction_in:
         # 1 in the pool
@@ -186,12 +234,14 @@ def find_inputs(
             # print(f"{'  ' * indent}Will be provided by {found_required[k]} {k}")
             required_in[k] += found_required[k]
 
-    print(f"required in: {required_in}")
-    print(f"pool: {pool}")
+    # print(f"required in: {required_in}")
+    # print(f"pool: {pool}")
     return required_in
 
 
 if __name__ == "__main__":
     _lines = read_raw_entries("input14.txt")
     part1(_lines)
+    part2(_lines)
+    # too high 1001464936982691503
     pass
