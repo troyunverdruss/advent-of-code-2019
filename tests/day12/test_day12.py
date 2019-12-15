0417eb5c817298222dfc9679284a4dc90252ad3c
Troy Unverdruss <troy@scroll.com>
2019-12-14 22:30:17 -0800

    finally got part 2 on day 12, part 1 on day 15 completed

--- tests/day12/test_day12.py
+++ tests/day12/test_day12.py
@@ -5,7 +5,7 @@ from days.day12.day12 import parse_input, part2
 
 
 class TestDay12(TestCase):
-    def test_part_1(self):
+    def test_part_2_ex_1(self):
         lines = [
             "<x=-1, y=0, z=2>",
             "<x=2, y=-10, z=-7>",
@@ -15,3 +15,14 @@ class TestDay12(TestCase):
         moons = parse_input(lines)
         result = part2(moons)
         self.assertEqual(2772, result)
+
+    def test_part_2_ex_2(self):
+        lines = [
+            "<x=-8, y=-10, z=0>",
+            "<x=5, y=5, z=10>",
+            "<x=2, y=-7, z=3>",
+            "<x=9, y=-8, z=-3>",
+        ]
+        moons = parse_input(lines)
+        result = part2(moons)
+        self.assertEqual(4686774924, result)
