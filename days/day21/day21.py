8cf512df8fc3443d24f33cd60fe80e02a6d19059
Troy Unverdruss <troy@scroll.com>
2019-12-21 19:28:24 -0800

    Day 21 part 2 done

--- days/day21/day21.py
+++ days/day21/day21.py
@@ -1,3 +1,5 @@
+aoc17() if 'aoc17' in dir() else None
+# from dataclasses import dataclass, field
 from typing import List
 
 from days.day02.intcode_computer import IntcodeComputer
@@ -8,39 +10,19 @@ def part1(instructions):
     ic = IntcodeComputer(instructions)
     ic.run()
     ic.outputs.clear()
-    # ic.inputs.extend(map(ord, list("OR D T\n")))
-    # ic.inputs.extend(map(ord, list("NOT A J\n")))
-    # ic.inputs.extend(map(ord, list("NOT A J\n")))
-    # ic.inputs.extend(map(ord, list("NOT B T\n")))
-    # ic.inputs.extend(map(ord, list("AND T J\n")))
-    # ic.inputs.extend(map(ord, list("NOT C T\n")))
-    # ic.inputs.extend(map(ord, list("AND T J\n")))
-    # ic.inputs.extend(map(ord, list("AND D J\n")))
 
-
-
-    #     # If A and B are empty, jump
-    # ic.inputs.extend(map(ord, list("NOT A J\n")))
-    # ic.inputs.extend(map(ord, list("NOT B T\n")))
-    # ic.inputs.extend(map(ord, list("AND J T\n")))
-    #
-    #
     # # If A is empty, jump
     ic.inputs.extend(map(ord, list("NOT A J\n")))
 
     # If C is empty but D isn't, jump
     ic.inputs.extend(map(ord, list("NOT C T\n")))
     ic.inputs.extend(map(ord, list("OR T J\n")))
-    # ic.inputs.extend(map(ord, list("NOT J T\n")))
-    # ic.inputs.extend(map(ord, list("OR J T\n")))
     ic.inputs.extend(map(ord, list("AND D J\n")))
-
 
     ic.inputs.extend(map(ord, list("WALK\n")))
 
     ic.waiting = False
     ic.run()
-
 
     for c in ic.outputs:
         try:
@@ -50,18 +32,51 @@ def part1(instructions):
         print(to_print, end="")
 
 
+def part2(instructions):
+    ic = IntcodeComputer(instructions)
+    ic.run()
+    ic.outputs.clear()
 
+    # # If C is empty but D isn't, jump
+    ic.inputs.extend(map(ord, list("NOT C T\n")))
+    ic.inputs.extend(map(ord, list("OR T J\n")))
 
+    # if H is blank, delay jumping
+    ic.inputs.extend(map(ord, list("AND H J\n")))
+
+    # if E is not blank, go ahead and jump
+    ic.inputs.extend(map(ord, list("NOT E T\n")))
+    ic.inputs.extend(map(ord, list("NOT T T\n")))
+    ic.inputs.extend(map(ord, list("OR T J\n")))
+
+    # If B is a hole
+    ic.inputs.extend(map(ord, list("NOT B T\n")))
+    ic.inputs.extend(map(ord, list("OR T J\n")))
+
+    # Failsafe, jump if A is empty ...
+    # # # If A is empty, jump
+    ic.inputs.extend(map(ord, list("NOT A T\n")))
+    ic.inputs.extend(map(ord, list("OR T J\n")))
+
+    # ... but don't jump if D is empty
+    ic.inputs.extend(map(ord, list("AND D J\n")))
+
+    ic.inputs.extend(map(ord, list("RUN\n")))
+
+    ic.waiting = False
+    ic.run()
+
+    for c in ic.outputs:
+        try:
+            to_print = chr(c)
+        except:
+            return c
+        print(to_print, end="")
 
 
 if __name__ == "__main__":
-    _inst = map(int, read_raw_entries("input21.txt")[0].split(","))
-    r1 = part1(_inst)
-    print(r1)
-    pass
-
-
-
-
-
-
+    _inst = list(map(int, read_raw_entries("input21.txt")[0].split(",")))
+    r1 = part1(_inst[:])
+    print(f"Part 1: {r1}")
+    r2 = part2(_inst[:])
+    print(f"Part 2: {r2}")
