7d6e9be36a0df99e845bbd4938d70c359a650ce8
Troy Unverdruss <troy@unverdruss.net>
2019-12-26 21:33:10 -0500

    Working forwards and backwards linear functions

--- days/day22/day22.py
+++ days/day22/day22.py
@@ -2,6 +2,7 @@ from collections import deque
 from dataclasses import dataclass
 from typing import List
 from helpers import read_raw_entries
+from sympy import mod_inverse
 
 
 def part1(lines, cards_count, target_card, cycles, reverse=False, initial_starting_order_list=None):
@@ -15,8 +16,9 @@ def part1(lines, cards_count, target_card, cycles, reverse=False, initial_starti
     else:
         deck.extend(initial_starting_order_list)
 
-    print(f"Cycle: {0}. Card at position 2020: {deck[2020]}")
+    # print(f"Cycle: {0}. Card at position 2020: {deck[2020]}")
     for cycle in range(cycles):
+        print(f"Cycle {cycle}: {deck}")
         for step in ordered_lines:
             if "deal with increment" in step:
                 count = int(step.split(" ")[3])
@@ -49,6 +51,7 @@ def part1_efficient(lines, cards_count, target_card, cycles):
     tailing = cards_count - target_card - 1
 
     for cycle in range(cycles):
+        print(f"Cycle {cycle}: {pos_target}")
         for step in lines:
             if "deal with increment" in step:
                 leading, pos_target, tailing = _deal_with_inc(cards_count, pos_target, step)
@@ -65,6 +68,60 @@ def part1_efficient(lines, cards_count, target_card, cycles):
                 raise Exception("Unknown command: ", step)
             print(f"Cycle {cycle + 1}, After step: {step}, target card at position: {pos_target}")
     return pos_target
+
+
+def part1_linear_functions(lines, cards_count, target_card):
+    # a, b, as in y = ax + b
+    # 1, 0 => exact same positions as it started/was
+    a, b = 1, 0
+
+    for step in lines:
+        if "deal with increment" in step:
+            inc = int(step.split(" ")[3])
+            temp_a = inc
+            temp_b = 0
+        elif "deal into new stack" in step:
+            temp_a = -1
+            temp_b = cards_count - 1
+        elif "cut" in step:
+            cut = int(step.split(" ")[1])
+            temp_a = 1
+            temp_b = -cut
+        else:
+            raise Exception("Unknown command: ", step)
+        # Combine the values from this step with the running tallies
+        a = (a * temp_a) % cards_count
+        b = (temp_a * b + temp_b) % cards_count
+
+    return (a * target_card + b) % cards_count
+
+
+def part1_linear_functions_reverse(lines, cards_count, target_card):
+    # a, b, as in y = ax + b
+    # 1, 0 => exact same positions as it started/was
+    a, b = 1, 0
+
+    reversed_lines = lines[:]
+    reversed_lines.reverse()
+    for step in reversed_lines:
+        if "deal with increment" in step:
+            inc = int(step.split(" ")[3])
+            temp_a = modinv(inc, cards_count)
+            temp_b = 0
+        elif "deal into new stack" in step:
+            temp_a = -1
+            temp_b = cards_count - 1
+        elif "cut" in step:
+            cut = int(step.split(" ")[1])
+            temp_a = 1
+            temp_b = cut
+        else:
+            raise Exception("Unknown command: ", step)
+        # Combine the values from this step with the running tallies
+        a = (a * temp_a) % cards_count
+        b = (temp_a * b + temp_b) % cards_count
+
+    return (a * target_card + b) % cards_count
 
 
 def _deal_with_inc(cards_count, pos_target, step):
@@ -84,16 +141,24 @@ def part2(lines, cards_count, final_target, cycles):
     pos_target = final_target
     tailing = cards_count - final_target - 1
 
+    pos_target_cycle = {}
+
     for cycle in range(cycles):
+        if cycle % 1_000_000 == 0:
+            print("Cycle ", cycle)
+        # print(f"Cycle: {cycle}")
         # if pos_target in pos_target_cycle.keys():
         #     print(f"Repeat found on cycle {cycle}")
         #     cycles_before_repeat = cycles
         #     period = cycles - pos_target_cycle[pos_target]
         #     remainder = (cycles - cycles_before_repeat) % period
-        #     return pos_on_cycle[remainder]
+        #     print(f"Period: {period}")
+        #     print(f"Accoutns for all but last {remainder}")
+        #     # return pos_on_cycle[remainder]
+        #
         #
         # pos_target_cycle[pos_target] = cycle
-        # pos_on_cycle[cycle] = pos_target
+        # # pos_on_cycle[cycle] = pos_target
 
         for step in ordered_lines:
             if "deal with increment" in step:
@@ -107,14 +172,10 @@ def part2(lines, cards_count, final_target, cycles):
                 leading = (leading + count) % cards_count
                 tailing = (tailing - count) % cards_count
                 pos_target = leading
-                # else:
-                #     leading = (leading + -1*count) % cards_count
-                #     tailing = (tailing - -1*count) % cards_count
-                #     pos_target = leading
             else:
                 raise Exception("Unknown command: ", step)
-            print(f"Cycle {cycle + 1}, After step: {step}, target card at position: {pos_target}")
-        print(f"Cycle: {cycle}. Position: {pos_target}")
+            # print(f"Cycle {cycle + 1}, After step: {step}, target card at position: {pos_target}")
+        # print(f"Cycle: {cycle}. Position: {pos_target}")
     return pos_target
 
 
@@ -132,17 +193,27 @@ class Multiples:
 def _reverse_deal_with_inc(cards_count, pos_target, step):
     count = int(step.split(" ")[3])
 
+    new_target = pos_target * modinv(count, cards_count) % cards_count
 
-    f_lead = f_pos = f_tail = -1
-    possible_target = -1
-    while f_pos != pos_target:
-        possible_target += 1
-        f_lead, f_pos, f_tail = _deal_with_inc(cards_count, possible_target, step)
+    leading = new_target
+    tailing = cards_count - new_target - 1
+    return leading, new_target, tailing
 
-    leading = pos_target
-    tailing = cards_count - possible_target - 1
-    return leading, possible_target, tailing
 
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
 
     #
     # # Find the inverse mod
@@ -162,10 +233,11 @@ def _reverse_deal_with_inc(cards_count, pos_target, step):
 if __name__ == "__main__":
     _lines = read_raw_entries("input22.txt")
 
-    r11 = part1(_lines, 10007, 2019, 10)
-    print(r11)
-    # r1 = part2(_lines, 10007, 2019, 1)
-    # print(r1)
+    # r11 = part1(_lines, 10007, 2019, 10007)
+    # print(r11)
 
-    # r2 = part2(_lines, 119315717514047, 2020, 10)
+    r1 = part2(_lines, 119315717514047, 2020, 17574135437386)
+    print(r1)
+
+    # r2 = part2(_lines, 119315717514047, 2020, 101741582076661)
     # print(r2)
